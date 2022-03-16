ebf5ba087e1fc5866fd548ad3794122e4eadf007
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-03-16 21:45:40 +0600

    update

--- Module-notes/Module-45/react-concepts/src/App.js
+++ Module-notes/Module-45/react-concepts/src/App.js
@@ -1,21 +1,68 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const products = [
-    { name:'laptop',price:100000 },
-    { name:'phone',price:12000 },
-    { name:'watch',price:2000 },
-    { name:'tablet',price:22000 },
-  ]
   return (
     <div className="App">
-      { 
+      {/* { 
       products.map(product => <Product name={product.name} price={product.price}></Product>)
-      }
+      } */}
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
+      <br />
     </div>
   );
 }
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(()=> {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  },[])
+  return (
+    <div>
+      <h1>External Users</h1>
+      {
+        users.map(user => <Users name ={user.name} email ={user.email}></Users>)
+      }
+    </div>
+  )
+}
+function Users(props){
+  return (
+    <div>
+      <h3>Name: {props.name}</h3>
+      <h4>Email: {props.email}</h4>
+    </div>
+  )
+}
 
+
+function Counter(){
+  const [count,setCount] =  useState(0);
+  
+  const increaseCount = () =>{
+    const newCount = count + 1;
+    setCount(newCount);
+  }
+  const decreaseCount = () => setCount(count - 1);
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick = {increaseCount}>Increase</button>
+      <br />
+      <br /> 
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  );
+}
+const products = [
+  { name:'laptop',price:100000 },
+  { name:'phone',price:12000 },
+  { name:'watch',price:2000 },
+  { name:'tablet',price:22000 },
+]
 function Product(props){
   return (
     <div className="product-container">
