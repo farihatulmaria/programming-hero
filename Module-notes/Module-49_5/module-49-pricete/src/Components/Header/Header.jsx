f1f79e61582891d5b145c777743d6ba5c50b2ce5
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-03-23 22:16:45 +0600

    header completed

--- Module-notes/Module-49_5/module-49-pricete/src/Components/Header/Header.jsx
+++ Module-notes/Module-49_5/module-49-pricete/src/Components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from '../../images/Logo.svg';
+import './Header.css';
 const Header = () => {
     return (
         <header>
             <nav>
                 <img src={logo} alt="eme-json" />
-                <ul>
+                <ul className='main-menu'>
                     <li><a href="/order">order</a></li>
                     <li><a href="/review">order review</a></li>
                     <li><a href="/inventory">manage inventory</a></li>
