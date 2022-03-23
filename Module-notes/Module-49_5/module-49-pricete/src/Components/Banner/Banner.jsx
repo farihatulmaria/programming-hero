40f41c9a43b62ad11845ee48c6c9879464abe709
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-03-23 22:44:25 +0600

    banner completed

---
+++ Module-notes/Module-49_5/module-49-pricete/src/Components/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import bannerImage from "../../images/banner-img.png";
+import './Banner.css';
+const Banner = () => {
+    return (
+        <section className='banner'>
+            <div className="banner-content">
+                <h4>Sale up to 70% off</h4>
+                <h1>New Collection For Fall</h1>
+                <p>Discover all the new arrivals of ready-to-wear collection.</p>
+                <button className='shop-btn'>shop now</button>
+            </div>
+            <div className="banner-image">
+                <img src={bannerImage} alt="" />
+            </div>
+        </section>
+    );
+};
+
+export default Banner;
