dba9c88b5b34ff74a3ffd14ada58397d0a3d7a29
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-03-23 23:21:11 +0600

    product container's product's css completed

--- Module-notes/Module-49_5/module-49-pricete/src/Components/Product/Product.jsx
+++ Module-notes/Module-49_5/module-49-pricete/src/Components/Product/Product.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './Product.css'
+import './Product.css';
 const Product = (props) => {
     const {product} = props;
-    const {name,id,price,seller,img,rating} = product 
+    const {name,price,seller,img,ratings} = product 
     return (
         <div className='product'>
             <img src={img} alt={name} />
@@ -10,7 +10,7 @@ const Product = (props) => {
                 <h2 className='product-name'>{name}</h2>
                 <h3 className='product-price'>Price: ${price}</h3>
                 <p className='product-seller'>Manufacturer : {seller}</p>
-                <p className='product-rating'>Rating: {rating} stars</p>
+                <p className='product-rating'>Rating: {ratings} stars</p>
             </div>
             <button className='cart-btn'>add to cart</button>
         </div>
