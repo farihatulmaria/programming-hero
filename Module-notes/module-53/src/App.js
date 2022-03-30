a204b93736d5f6368b6508f824a3c37925da394e
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-03-30 22:14:30 +0600

    setting up
    setting up the module for coding and addng react-router to the project

---
+++ Module-notes/module-53/src/App.js
@@ -0,0 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import Orders from './components/Orders/Orders';
+import Shop from './components/Shop/Shop';
+
+function App() {
+  return (
+    <>
+    <Header></Header>
+      <Routes>
+        <Route path='/' element={<Shop></Shop>}></Route>
+        <Route path='/home' element={<Shop></Shop>}></Route>
+        <Route path='/shop' element={<Shop></Shop>}></Route>
+        <Route path='/orders' element={<Orders></Orders>}></Route>
+        <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+      </Routes>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ Module-notes/module-53/src/components/Inventory/Inventory.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h3>from Inventory component</h3>
+        </div>
+    );
+};
+
+export default Inventory;
