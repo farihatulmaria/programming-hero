9cb5561c98a3179470648957289a22fd83bbdff0
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-04-09 22:20:37 +0600

    module-59

---
+++ Module-notes/module-59/ema-jhone-auth/src/__auth_steps.js
@@ -0,0 +1,29 @@
+/* 
+
+    1. Create a new project on https://console.firebase.google.com/
+
+    2. Register the project
+
+    3. install firebase
+
+    4. add firebase config in src folder and export auth
+    
+    5. firebase settings > Authentication > enable providers
+
+    6. 
+
+
+*/
+
+/* hosting steps 
+
+ 1. npm install -g firebase-tools
+ 2. firebase login
+ 3. firebase init
+ 4. firebase deploy
+
+
+
+
+
+*/
