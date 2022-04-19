bc187c25e07afe872ef513e580173e564045eec6
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-04-19 21:31:34 +0600

    update

---
+++ Module-notes/Module-64/note.js
@@ -0,0 +1,67 @@
+/* 
+Node JS Creator Ryan Dahl in 2009
+
+Node js is an asynchronous event-driven JavaScript runtime, Node.js is designed to build scalable network applications.
+
+or, 
+
+Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on the V8 engine and executes JavaScript code outside a web browser. 
+
+or,
+Node.js is a runtime environment that's used to run JavaScript outside the browser.
+
+
+Node VS JS
+
+JavaScript is a simple programming language that runs in any browser JavaScript Engine. Whereas Node JS is an interpreter or running environment for a JavaScript programming language that holds many excesses, it requires libraries that can easily be accessed from JavaScript programming for better use.
+
+
+
+
+
+When to use node js
+
+Node. js is primarily used for non-blocking, event-driven servers, due to its single-threaded nature. It's used for traditional web sites and back-end API services, but was designed with real-time, push-based architectures in mind.
+
+
+why should i use node
+
+Node. js is easily employed as a server-side proxy where it can handle a large amount of simultaneous connections in a non-blocking manner. It's especially useful for proxying different services with different response times, or collecting data from multiple source points.
+
+
+
+
+Express js - Fast, unopinionated, minimalist web framework for Node.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
