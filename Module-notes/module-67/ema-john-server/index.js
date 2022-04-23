bb9e2db2532e6a100561305badb27d2a1195f178
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-04-23 22:50:49 +0600

    update

--- Module-notes/module-67/ema-john-server/index.js
+++ Module-notes/module-67/ema-john-server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +8,23 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://ema-john-db-user:oo5lfvj8eERLSZF5@cluster0.fvvsv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+/* client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+ */
+
+
+
+
+
 
 
 
---
+++ Module-notes/module-67/note.js
@@ -0,0 +1,70 @@
+/* 
+
+GET VS POST
+
+
+
+The GET method requests a representation of the specified resource. Requests using GET should only retrieve data.
+
+The POST method submits an entity to the specified resource, often causing a change in state or side effects on the server.
+
+
+1.
+-GET retrieves a representation of the specified resource.
+
+-POST is for writing data, to be processed to the identified resource.
+
+2.
+
+-It typically has relevant information in the URL of the request.
+
+-It typically has relevant information in the body of the request.
+
+3.
+
+-It is limited by the maximum length of the URL supported by the browser and web server.
+
+-It does not have such limits.
+
+4.
+
+-It is the default HTTP method.
+
+-In this, we need to specify the method as POST to send a request with the POST method.
+
+5.
+
+-You can bookmark GET requests.
+
+-You cannot bookmark POST requests.
+
+6.
+
+-It is less secure because data sent is part of the URL
+
+-It is a little safer because the parameters are not stored in browser history or in web server logs.
+
+7.
+
+-It is cacheable.
+
+-It is not cacheable.
+
+8.
+
+-For eg. GET the page showing a particular question.
+
+-For eg. Send a POST request by clicking the “Add to cart” button.
+
+
+
+
+
+
+
+
+
+
+
+
+*/
