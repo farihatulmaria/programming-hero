9a7a1bd6fcb7c89067ba9168a045c8b226085d49
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-05-10 23:47:51 +0600

    added a appointment page

--- Module-notes/Module-71/doctor-portal/src/App.js
+++ Module-notes/Module-71/doctor-portal/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home';
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/home' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
+        <Route path='/appointment' element={<Appointment/>}/>
       </Routes>
       <Footer/>
     </div>
