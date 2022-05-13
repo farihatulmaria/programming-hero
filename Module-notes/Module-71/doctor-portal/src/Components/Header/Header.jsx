889d876fe8b522664b68a241ae17344ae1b49f90
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-05-13 17:11:17 +0600

    added a sign out button on the header for signing out

--- Module-notes/Module-71/doctor-portal/src/Components/Header/Header.jsx
+++ Module-notes/Module-71/doctor-portal/src/Components/Header/Header.jsx
@@ -1,14 +1,20 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase';
 
 const Header = () => {
+    const [user] = useAuthState(auth);
     const menuItems = <>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/about'>About</Link></li>
         <li><Link to='/appointment'>Appointment</Link></li>
         <li><Link to='/reviews'>Reviews</Link></li>
         <li><Link to='/contact'>Contact Us</Link></li>
-        <li><Link to='/login'>Login</Link></li>
+        {
+            user?.uid ?<li><button onClick={()=> signOut(auth)}>Sign Out</button></li>:<li><Link to='/login'>Login</Link></li>
+        }
     </>
     return (
         <div className="navbar bg-base-100 p-5">
