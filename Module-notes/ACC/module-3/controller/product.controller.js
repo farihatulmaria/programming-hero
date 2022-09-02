644c43fe1f08f1ce74b84953571092dbfefedf4e
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-09-02 13:11:22 +0600

    added some notes to the file

--- Module-notes/ACC/module-3/controller/product.controller.js
+++ Module-notes/ACC/module-3/controller/product.controller.js
@@ -1,42 +1,35 @@
-module.exports.getAllProducts = async (req,res)=>{
-    /** for production level programming / programming with a team
-         * @api {get} /get All product
-         * @apiDescription Get all the product
-         * @apiPermission pubilc
-         *
-         * @apiHeader {String} Authorization   User's access token
-         *
-         * @apiParam  {Number{1-}}         [page=1]     List page
-         * @apiParam  {Number{1-100}}      [limit=10]  Users per page
-         *
-         * @apiSuccess {Object[]} all the tools.
-         *
-         * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
-         * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
-    */ 
+module.exports.getAllProducts = async (req,res,next)=>{
+    /* #note req:-
+    The req object represents the HTTP request and has properties for the request query string, parameters, body, HTTP headers, and so on. The object is always referred to as req (and the HTTP response is res) but its actual name is determined by the parameters to the callback function in which you’re working.
+     */
+    const {ip ,params, body,headers}=req;
+    const reqArray =[ip,params,body,headers];
+    // console.log(reqArray);
+
+    /* #note res:-
+    The res object represents the HTTP response that an Express app sends when it gets an HTTP request.The object is always referred to as res (and the HTTP request is req) but its actual name is determined by the parameters to the callback function in which you’re working. */
     res.send('products founded')
 }
 module.exports.addAProduct = async (req,res)=>{
-    /** for production level programming / programming with a team
-         * @api {get} / saves a product
-         * @apiDescription Get all the product
-         * @apiPermission admin
-         *
-         * @apiHeader {String} Authorization   User's access token
-         *
-         * @apiParam  {Number{1-}}         [page=1]     List page
-         * @apiParam  {Number{1-100}}      [limit=10]  Users per page
-         *
-         * @apiSuccess {Object[]} all the tools.
-         *
-         * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
-         * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
-    */ 
     res.send('products added')
 }
+
+module.exports.getAProduct= async (req,res)=>{
+    const id = req.params.id;
+    res.send(`a product founded with id  no:${id}`)
+}
+module.exports.updateAProduct = async (req,res)=>{
+    res.send('products added')
+}
+
+
+
+
+
 /* you can do it like this module.exports= {getAllProducts,addAProduct}
 
    or, you can do it like this:
 
    module.exports.functionName(functionality) 
 */ 
+
--- Module-notes/ACC/module-3/routes/version-1/products.route.js
+++ Module-notes/ACC/module-3/routes/version-1/products.route.js
@@ -16,17 +16,27 @@ router.post('/' , async (req,res)=>{
 
     // or like this:
 router
-    .route('/')   
+    .route('/')
         .get(productController.getAllProducts)
-        .post(productController.addAProduct)
+        .post(productController.addAProduct
+            /** for production level programming / programming with a team -it isn't that helpful
+             * @api {get} / saves a product
+             * @apiDescription Get all the product
+             * @apiPermission admin
+             *
+             * @apiHeader {String} Authorization   User's access token
+             *
+             * @apiParam  {Number{1-}}         [page=1]     List page
+             * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+             *
+             * @apiSuccess {Object[]} all the tools.
+             *
+             * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+             * @apiError (Forbidden 403)     Forbidden     Only admins can access the data*/ 
+        )
 router
     .route('/:id')
-        .get(async (req,res)=>{
-            const id = req.params.id;
-            res.send(`a product founded with id  no:${id}`)
-        })
-        .post(async (req,res)=>{
-            res.send('products added')
-        })
+        .get(productController.getAProduct)
+        .post(productController.updateAProduct)
 
 module.exports = router;
